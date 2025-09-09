434a09a7c6e8920ad7885a8f9c1b9aeacbf9e2c8
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-09-09 22:13:02 +0500

    Task1

---
+++ Tyuiu.KozyrevRA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KozyrevRA.Sprint0.Task0.V_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Роман";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
